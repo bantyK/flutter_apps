75ef03b5f1d579000263d235e1df5e40d0f5d9e3
bantyK <bantyp92@gmail.com>
2019-05-19 13:07:45 +0530

    Calculate and Reset functions added

--- simple_interest_calculator_app/lib/screens/main_screen.dart
+++ simple_interest_calculator_app/lib/screens/main_screen.dart
@@ -11,6 +11,11 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   var _currencies = ['Rupees', 'Dollars', 'Pounds'];
   var _selectedCurrencyValue = 'Rupees';
+  var amountOutput = '';
+
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController termsController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +34,7 @@ class _MainScreenState extends State<MainScreen> {
               padding: const EdgeInsets.only(top: 16.0),
               child: TextField(
                 keyboardType: TextInputType.number,
+                controller: principalController,
                 style: textStyle,
                 decoration: InputDecoration(
                     labelText: 'Principal Amount',
@@ -41,6 +47,7 @@ class _MainScreenState extends State<MainScreen> {
               padding: const EdgeInsets.only(top: 16.0),
               child: TextField(
                 keyboardType: TextInputType.number,
+                controller: roiController,
                 style: textStyle,
                 decoration: InputDecoration(
                   labelText: 'Rate of Interest',
@@ -59,6 +66,7 @@ class _MainScreenState extends State<MainScreen> {
                     child: TextField(
                       keyboardType: TextInputType.number,
                       style: textStyle,
+                      controller: termsController,
                       decoration: InputDecoration(
                         labelText: 'Rate of Interest',
                         hintText: 'In percentage',
@@ -81,7 +89,7 @@ class _MainScreenState extends State<MainScreen> {
                       }).toList(),
                       value: _selectedCurrencyValue,
                       onChanged: (String selectedValue) {
-                        _selectedCurrencyValue = selectedValue;
+                        _onDropDownItemSelected(selectedValue);
                       },
                     ),
                   )
@@ -98,7 +106,11 @@ class _MainScreenState extends State<MainScreen> {
                       child: RaisedButton(
                         color: Theme.of(context).accentColor,
                         textColor: Theme.of(context).primaryColorDark,
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            this.amountOutput = _calculateTotalReturn();
+                          });
+                        },
                         child: Text(
                           'Calculate',
                           textScaleFactor: 1.2,
@@ -109,7 +121,11 @@ class _MainScreenState extends State<MainScreen> {
                       child: RaisedButton(
                         color: Theme.of(context).primaryColorDark,
                         textColor: Theme.of(context).primaryColorLight,
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            clear();
+                          });
+                        },
                         child: Text(
                           'Reset',
                           textScaleFactor: 1.2,
@@ -123,7 +139,7 @@ class _MainScreenState extends State<MainScreen> {
             Padding(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                "This will be invisible",
+                this.amountOutput,
                 textAlign: TextAlign.center,
                 style: textStyle,
               ),
@@ -132,5 +148,29 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
     );
+  }
+
+  void _onDropDownItemSelected(String selectedValue) {
+    setState(() {
+      this._selectedCurrencyValue = selectedValue;
+    });
+  }
+
+  String _calculateTotalReturn() {
+    double principleAmount = double.parse(principalController.text);
+    double roi = double.parse(roiController.text);
+    double terms = double.parse(termsController.text);
+
+    double totalAmountPayable =
+        principleAmount + ((principleAmount * roi * terms) / 100);
+
+    return "Total amount: $totalAmountPayable";
+  }
+
+  void clear() {
+    this.principalController.clear();
+    this.roiController.clear();
+    this.termsController.clear();
+    this.amountOutput = '';
   }
 }
